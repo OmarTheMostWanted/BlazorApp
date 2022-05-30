5fb49d8e1541a402f9cf65617d01d365d7225be1
OmarTheMostWanted <45181439+OmarTheMostWanted@users.noreply.github.com>
2022-05-30 22:44:21 +0200

    added CancellationTokens

--- WebApp/Client.cs
+++ WebApp/Client.cs
@@ -12,34 +12,34 @@ public class Client
         _client.BaseAddress = new Uri("https://localhost:7121/");
     }
 
-    public async Task<bool> SetX(double x)
+    public async Task<bool> SetX(double x , CancellationToken cancellationToken = default)
     {
         var httpRequest = new HttpRequestMessage(HttpMethod.Post, $"/calculator/SetX/{x}");
-        var response = await _client.SendAsync(httpRequest, CancellationToken.None);
+        var response = await _client.SendAsync(httpRequest, cancellationToken);
         return response.IsSuccessStatusCode;
     }
     
     
-    public async Task<bool> SetY(double y)
+    public async Task<bool> SetY(double y, CancellationToken cancellationToken = default)
     {
         var httpRequest = new HttpRequestMessage(HttpMethod.Post, $"/calculator/SetY/{y}");
-        var response = await _client.SendAsync(httpRequest, CancellationToken.None);
+        var response = await _client.SendAsync(httpRequest , cancellationToken);
         return response.IsSuccessStatusCode;
     }
     
     
-    public async Task<bool> SetOp(char op)
+    public async Task<bool> SetOp(char op, CancellationToken cancellationToken = default)
     {
         var httpRequest = new HttpRequestMessage(HttpMethod.Post, $"/calculator/SetOp{op}");
-        var response = await _client.SendAsync(httpRequest, CancellationToken.None);
+        var response = await _client.SendAsync(httpRequest, cancellationToken);
         return response.IsSuccessStatusCode;
     }
 
 
-    public async Task<double> Calculate()
+    public async Task<double> Calculate(CancellationToken cancellationToken = default)
     {
         var httpRequest = new HttpRequestMessage(HttpMethod.Get, $"/calculator/res");
-        var response = await _client.SendAsync(httpRequest, CancellationToken.None);
+        var response = await _client.SendAsync(httpRequest, cancellationToken);
         if (response.IsSuccessStatusCode)
         {
             return await response.Content.ReadFromJsonAsync<double>();
@@ -48,10 +48,10 @@ public class Client
         return 0;
     }
 
-    public async Task<bool> Reset()
+    public async Task<bool> Reset(CancellationToken cancellationToken = default)
     {
         var httpRequest = new HttpRequestMessage(HttpMethod.Delete, "calculator/reset");
-        var response =  await _client.SendAsync(httpRequest, CancellationToken.None);
+        var response =  await _client.SendAsync(httpRequest,cancellationToken);
         return response.IsSuccessStatusCode;
     }
 
